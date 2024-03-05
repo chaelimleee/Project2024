cb5ad980d1e29a1e863d7268e483580deb470a5b
chaelimleee <cofla1007@gmail.com>
2024-03-05 11:31:52 +0900

    message

---
+++ src/main/java/com/javateam/STDProject/StdProject0305Application.java
@@ -0,0 +1,13 @@
+package com.javateam.STDProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StdProject0305Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StdProject0305Application.class, args);
+	}
+
+}
